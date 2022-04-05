f84c2d0dd386ab2586a143e80e7b8246155bf655
Ana Paula Leão <paula.leao@gmail.com>
2022-04-04 21:30:30 -0300

    nova main

---
+++ src/contexts/Auth/AuthProvider.tsx
@@ -0,0 +1,34 @@
+import { Children,  useState } from "react";
+import { useApi } from "../../hooks/useAPI";
+import { User } from "../../types/User";
+import { AuthContext } from "./AuthContext";
+
+export const AuthProvider =({children}:{children: JSX.Element})=>{
+    const[user,setUser] = useState<User | null>(null);
+    const api = useApi();
+
+    const signIn = async (email:string , password: string) =>{
+        const data= await api.signIn(email,password);
+        if (data.user && data.token){
+            setUser(data.user);
+            return true
+        }
+        return false;
+    }
+
+    const signOut =async () =>{
+        await api.logout();
+        setUser(null);
+
+    }
+
+
+
+
+    return(
+        
+        <AuthContext.Provider value={{user, signIn, signOut}}>
+            {Children}
+        </AuthContext.Provider>
+    )
+}
